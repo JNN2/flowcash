3c8c23e132f13f54249b3b87fce516a3665bf058
SeuNomeNoGitHub <seuemail@exemplo.com>
2025-10-27 19:03:02 -0300

    Primeiro commit do projeto FlowCash

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("O script.js foi carregado com sucesso! A página está pronta.");
+
+  document.body.classList.add('loaded');
+});
+
