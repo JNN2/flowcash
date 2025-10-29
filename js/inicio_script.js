97f1f5ee3dc4e256774245857e839c62d2c6de91
SeuNomeNoGitHub <seuemail@exemplo.com>
2025-10-29 20:07:04 -0300

    Corrige posição da imagem no header

---
+++ js/inicio_script.js
@@ -0,0 +1,61 @@
+function abrirNotificacoes() {
+    console.log("Botão Notificações clicado!");
+}
+
+function abrirConfiguracoes() {
+    console.log("Botão Configurações clicado!");
+
+}
+
+function abrirPerfil() {
+    console.log("Botão Perfil clicado!");
+
+}
+function adicionarDespesas() {
+    console.log("Botão Despesas clicado!");
+
+}
+
+function adicionarReceitas() {
+    console.log("Botão Receitas clicado!");
+}
+
+function abrirRelatorio() {
+    console.log("Botão Relatorio clicado!");
+}
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-item a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            // Se o link levar para uma URL real, remova o e.preventDefault()
+            e.preventDefault(); 
+            
+            // Remove a classe 'active' de todos os itens
+            document.querySelectorAll('.nav-item').forEach(item => item.classList.remove('active'));
+            
+            // Adiciona a classe 'active' ao item clicado (o pai <li>)
+            e.target.parentElement.classList.add('active');
+            
+            // Lógica de Redirecionamento (exemplo)
+            // window.location.href = e.target.href; 
+        });
+    });
+});
+
+const toggle = document.getElementById("toggleSaldo");
+const saldo = document.getElementById("saldoDisplay");
+const icon = toggle.querySelector("i");
+
+toggle.addEventListener("click", () => {
+    const isHidden = saldo.classList.toggle("hidden-saldo");
+
+    // Alterna o ícone e o estilo ativo
+    if (isHidden) {
+        icon.classList.replace("fa-eye", "fa-eye-slash");
+        toggle.classList.add("active");
+    } else {
+        icon.classList.replace("fa-eye-slash", "fa-eye");
+        toggle.classList.remove("active");
+    }
+});
