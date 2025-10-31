cda0dd3983359d7b969e69224bb89fad3ded6715
zCaua <cauamatheus150@gmail.com>
2025-10-31 16:54:44 -0300

    Add files via upload
    JavaScript, para apenas checagem dos botões dentro do console.

---
+++ js/lançamentos_script.js
@@ -0,0 +1,31 @@
+function abrirNotificacoes() {
+    console.log("Botão Notificações clicado!");
+}
+
+function abrirConfiguracoes() {
+    console.log("Botão Configurações clicado!");
+}
+
+function abrirPerfil() {
+    console.log("Botão Perfil clicado!");
+}
+
+function adicionarLançamento() {
+    console.log("Botão lançamento clicado!");
+}
+
+function mesAnterior() {
+    console.log("Seta voltar clicada!");
+}
+
+function mesSeguinte() {
+    console.log("Seta seguir clicada!");
+}
+
+function trocarConta() {
+    console.log("Botão trocarconta clicado!");
+}
+
+function maisOpçoes() {
+    console.log("Botão de mais opções clicado!");
+}
